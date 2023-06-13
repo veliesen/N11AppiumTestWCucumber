8830421439c665cd8487180d80afb4b239fe6a93
DESKTOP-DFDCC26\90506 <veliesenamzn@gmail.com>
2023-06-13 22:27:09 +0300

    N11 Mobile App Test Cases Developed By Cucumber BDD Approach

--- appium-mobile-automation/src/main/java/Pages/AppiumTestSteps.java
+++ appium-mobile-automation/src/main/java/Pages/AppiumTestSteps.java
@@ -2,30 +2,42 @@ package Pages;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.offset.PointOption;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+
+
+import javax.naming.directory.SearchControls;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class AppiumTestSteps {
 
     private AppiumDriver driver;
+    private MobileElement element;
+    private MobileElement el;
 
-    @Given("Selendroid APK OPEN")
-    public void selendroid_APK_basariyla_yuklendi() {
+    @Given("N11 AndroidApp APK Open")
+    public void N11_APK_SetUP() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("appium:appActivity", "io.selendroid.testapp.HomeScreenActivity");
-        desiredCapabilities.setCapability("appium:appPackage", "io.selendroid.testapp");
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("appium:deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("appium:appActivity", "com.dmall.mfandroid.activity.base.NewSplash");
+        desiredCapabilities.setCapability("appium:appPackage", "com.dmall.mfandroid");
+        desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
         desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
         desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
         try {
             driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
         } catch (MalformedURLException e) {
@@ -33,45 +45,141 @@ public class AppiumTestSteps {
         }
     }
 
-    @When("Continue button clicked")
-    public void continueClick() {
-        driver.findElement(By.id("com.android.permissioncontroller:id/continue_button")).click();
+    @When("Click On Login Icon On Homepage")
+    public void clickOnLoginIcon() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.xpath("//android.widget.FrameLayout[@content-desc=\"Hesabım\"]/android.widget.FrameLayout/android.widget.ImageView")).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Then("Click On SignIn Button")
+    public void clickOnSignInButton(){
+        driver.findElement(By.id("com.dmall.mfandroid:id/btn_login")).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @And("Send Valid Email {string}")
+    public void sendValidEmail(String string) throws InterruptedException{
+        Thread.sleep(1000);
+        byte[] decryptedPasswordInBytes= Base64.getDecoder().decode(string.getBytes(StandardCharsets.UTF_8)); //Decrypt The Encrypted Email
+        String decryptedPassword =new String((decryptedPasswordInBytes));
+        Thread.sleep(1000);
+       element= (MobileElement) driver.findElement(By.id("com.dmall.mfandroid:id/loginEmailET"));
+       element.sendKeys(decryptedPassword);
+
     }
 
-    @When("Selendroid info popup closed")
-    public void infoPopup() {
-        new TouchAction(driver).tap(PointOption.point(401, 539)).perform();
+    @And("Send Valid Password {string}")
+    public void sendValidPassword(String string) throws InterruptedException{
+        Thread.sleep(1000);
+        byte[] decryptedPasswordInBytes= Base64.getDecoder().decode(string.getBytes(StandardCharsets.UTF_8));  //Decrypt The Encrypted Password
+        String decryptedPassword =new String((decryptedPasswordInBytes));
+        Thread.sleep(1000);
+        element= (MobileElement) driver.findElement(By.id("com.dmall.mfandroid:id/loginPassET"));
+        element.sendKeys(decryptedPassword);
     }
+    @And("Click On Login Button")
+    public void clickOnLoginInButton() {
+        driver.findElement(By.id("com.dmall.mfandroid:id/LoginBtn")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-    @When("Input sendkeys {string}")
-    public void uygulama_basariyla_aciliyor(String string) throws InterruptedException {
-        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("my_text_fieldCD");
-        el1.sendKeys(string);
+    }
+    @And("Click On Homepage Icon")
+    public void clickToNavigateHomepage() {
+        driver.findElement(By.xpath("//android.widget.FrameLayout[@content-desc=\"Ana Sayfa\"]/android.widget.FrameLayout/android.widget.ImageView ")).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @And("Send Product Name {string}")
+    public void sendProductNameSearch(String string) throws InterruptedException {
+        element= (MobileElement) driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.LinearLayout[1]/android.view.ViewGroup/android.widget.TextView"));
+        element.click();
+
         Thread.sleep(3000);
-    }
 
-    @When("Display Text View")
-    public void displayTextView() throws InterruptedException {
-        MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("io.selendroid.testapp:id/visibleButtonTest");
+        el=(MobileElement) driver.findElement(By.id("com.dmall.mfandroid:id/etSearch"));
+        el.sendKeys(string);
+
+        Thread.sleep(2000);
+        Actions action=new Actions(driver);
+        action.moveToElement(el).sendKeys(Keys.ENTER).build().perform();
         Thread.sleep(3000);
-        el2.click();
+
+    }
+    @And("Click On Product")
+    public void clickOnProduct() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.view.ViewGroup/android.view.ViewGroup/androidx.appcompat.widget.LinearLayoutCompat/androidx.appcompat.widget.LinearLayoutCompat/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.ImageView")).click();
+        Thread.sleep(2000);
     }
 
-    @When("Displays a Toast")
-    public void displayAToast() {
-        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("io.selendroid.testapp:id/showToastButton");
-        el1.click();
+    @And("Click On Add Product Button")
+    public void clickOnAddCart() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.findElement(By.id("com.dmall.mfandroid:id/pdpAddToCartButton")).click();
+        Thread.sleep(2000);
+    }
+    @And("Click On Delete Product Icon Delete From Cart")
+    public void clickOnDeleteProduct() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.findElement(By.id("com.dmall.mfandroid:id/firstItemView")).click();
+        Thread.sleep(2000);
+    }
+    @And("Click On Delete And Add Product to Favorites PopUp")
+    public void clickOnAddProductFavorites() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.findElement(By.id("com.dmall.mfandroid:id/deleteAndAddFavoriteTv")).click();
+        Thread.sleep(2000);
+    }
+    @And("Check Welcoming Text {string}")
+    public void checkCartEmpty(String string) throws InterruptedException{
+        Thread.sleep(2000);
+        String expectedText=driver.findElement(By.id("com.dmall.mfandroid:id/emptyBasketTitleTv")).getText();
+        Assert.assertEquals(expectedText,string);
+
+        Thread.sleep(2000);
+    }
+    @And("Click On Settings Icon On Account Page To Navigate LogOut Page")
+    public void clickOnSettingsIcon() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.findElement(By.id("com.dmall.mfandroid:id/iv_account_settings")).click();
+        Thread.sleep(2000);
+    }
+    @And("Click On LogOut")
+    public void clickOnLogOut() throws InterruptedException{
+        driver.findElement(By.id("com.dmall.mfandroid:id/accountFragmentLogOutIV")).click();
+        Thread.sleep(2000);
+    }
+    @And("Click On Yes LogOut on PopUp")
+    public void clickOnLogOutSurePopUp() throws InterruptedException{
+        driver.findElement(By.id("com.dmall.mfandroid:id/customInfoDialogBtn1")).click();
+        Thread.sleep(2000);
+    }
+    @And("Check {string} Whether LogOut Complete")
+    public void checkLogOutCompletion(String string)throws InterruptedException{
+        Thread.sleep(1000);
+        String expectedText=driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.LinearLayout[2]/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.TextView[1]")).getText();
+        Assert.assertEquals(expectedText,string);
     }
 
-    @When("Displays popup window button click")
-    public void displayPopupWindow() {
-        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("io.selendroid.testapp:id/showPopupWindowButton");
-        el1.click();
-    }
-
-    @When("Press to throw unhandled exception")
-    public void pressToException() {
-        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("io.selendroid.testapp:id/exceptionTestButton");
-        el1.click();
-    }
 }
+
+
--- appium-mobile-automation/src/main/resources/Login.feature
+++ appium-mobile-automation/src/main/resources/Login.feature
@@ -1,28 +1,37 @@
-Feature: Selendroid APK TEST CASES
 
-  Scenario: Test Case TC0001
-    Given Selendroid APK OPEN
-    When Continue button clicked
-    And Selendroid info popup closed
-    And Input sendkeys "TestQA"
+Feature: N11 AndroidApp Test Scenarios
+
+  Scenario: TC0001 Successful LogIn And LogOut
+    Given N11 AndroidApp APK Open
+    When Click On Login Icon On Homepage
+    Then Click On SignIn Button
+    And Send Valid Email "bW1vdmVzZW4xQGdtYWlsLmNvbQ=="
+    And Send Valid Password "QXR3YTE5ODU"
+    And Click On Login Button
+    And Click On Login Icon On Homepage
+    And Click On Settings Icon On Account Page To Navigate LogOut Page
+    And Click On LogOut
+    And Click On Yes LogOut on PopUp
 
 
-  Scenario: Test Case TC0002
-    Given Selendroid APK OPEN
-    When Continue button clicked
-    And Selendroid info popup closed
-    And Input sendkeys "TestQA"
-    And Display Text View
-    And Displays a Toast
-    And Displays popup window button click
+  Scenario: TC0002 Add A Product To Cart Them Delete Cart And LogOut
+    Given N11 AndroidApp APK Open
+    When Click On Login Icon On Homepage
+    Then Click On SignIn Button
+    And Send Valid Email "bW1vdmVzZW4xQGdtYWlsLmNvbQ=="
+    And Send Valid Password "QXR3YTE5ODU"
+    And Click On Login Button
+    And Click On Homepage Icon
+    And Send Product Name "Amator Denizcilikte Acil Durum Seyri"
+    And Click On Product
+    And Click On Add Product Button
+    And Click On Delete Product Icon Delete From Cart
+    And Check Welcoming Text "Sepetiniz Boş Görünüyor"
+    And Click On Login Icon On Homepage
+    And Click On Settings Icon On Account Page To Navigate LogOut Page
+    And Click On LogOut
+    And Click On Yes LogOut on PopUp
+    And Check "Üye Ol / Giriş Yap" Whether LogOut Complete
 
-  Scenario: Test Case TC0003
-    Given Selendroid APK OPEN
-    When Continue button clicked
-    And Selendroid info popup closed
-    And Input sendkeys "TestQA"
-    And Display Text View
-    And Displays a Toast
-    And Displays popup window button click
-    And Press to throw unhandled exception
+
 
